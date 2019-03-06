9f382b6d66626435e7650d4033449e2a5c647602
Zolon Wilkins <zolonsk8@gmail.com>
2019-03-06 11:03:45 -0800

    cleanup

--- app.js
+++ app.js
@@ -9,15 +9,12 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
-
-
-const newsXml = "https://www.news.com.au/content-feeds/latest-news-technology/"
-
 app.get('/', (req, res) => {
   res.render('pages/index');
 })
 
 app.get('/api/news', async (req, res) => {
+  const newsXml = "https://www.news.com.au/content-feeds/latest-news-technology/"
   try {
     var news = await modules.fetchXml(newsXml)
     parseString(news, function(err, result) {
