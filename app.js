1ee18649be5c202e0d41ab5a660c58ec45438ccc
Zolon Wilkins <zolonsk8@gmail.com>
2019-03-06 10:12:57 -0800

    news parsing

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const morgan = require("morgan");
+const parseString = require('xml2js').parseString;
+const axios = require("axios");
+
+const fetchXml = async url => {
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch(e) {
+    return e
+  }
+}
+
+const parseReponse = items => {
+  try {
+    const newsItems = items.map(function(i) {
+      return {
+        title: i.title[0],
+        description: i.description[0],
+        image: i.image[0].url[0],
+        url: i.link[0]
+      }
+    })
+    return newsItems
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const newsXml = "https://www.news.com.au/content-feeds/latest-news-technology/"
+
+app.get('/api/news', async (req, res) => {
+  try {
+    var news = await fetchXml(newsXml)
+    parseString(news, function(err, result) {
+      const items = parseReponse(result.rss.channel[0].item)
+      res.json(items)
+    })
+  } catch (e) {
+    res.json({})
+  }
+});
+
+app.use(morgan('dev'));
+app.listen(process.env.PORT || 3000);
